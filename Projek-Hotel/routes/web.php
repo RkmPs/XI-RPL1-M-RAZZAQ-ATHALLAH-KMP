b9b095639ff23e6f3c0b1097cfa2810a4107b3a2
RkmPs <140313382+RkmPs@users.noreply.github.com>
2024-10-15 21:22:50 +0700

    initial commit

---
+++ Projek-Hotel/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/tes', function () {
+    return view('tes');
+});
