38421f51bab108aa61fc43ebe4e4ed9f1682557e
RkmPs <140313382+RkmPs@users.noreply.github.com>
2024-10-16 18:41:40 +0700

    add auth feature

--- Projek-Hotel/routes/web.php
+++ Projek-Hotel/routes/web.php
@@ -1,12 +1,26 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\RoleCheck;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/tes', function () {
-    return view('tes');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware('role:super admin,admin')->group(function () {
+     Route::get('admin.dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+});
+
+require __DIR__.'/auth.php';
